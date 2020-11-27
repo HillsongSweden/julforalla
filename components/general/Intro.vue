ba09e2b3cf5129fd86fb8ddf31c2f7dc42683aed
Josef Lekardal <josef@workbuster.se>
2020-11-27 17:28:18 +0100

    add content component

--- components/general/Intro.vue
+++ components/general/Intro.vue
@@ -1,6 +1,9 @@
 <template>
   <header class="main flex flex-col justify-center">
-    <div class="sticky-header fixed top-0 left-0 w-full p-8 bg-cream z-20 shadow-sm" :class="{ active: isActive }">
+    <div
+      class="sticky-header fixed top-0 left-0 w-full py-5 px-10 bg-cream z-20 shadow-sm"
+      :class="{ active: isActive }"
+    >
       <div class="max-w-5xl mx-auto flex justify-between">
         <nuxt-image class="julforalla" src="/ogp.png" alt="En jul för alla" />
 
@@ -10,11 +13,12 @@
 
     <nuxt-image src="/ogp.png" alt="En jul för alla" class="julforalla" />
 
-    <article class="mx-auto text-lg leading-6">
+    <article class="mx-auto">
       <p>
-        Julen är en tid som ska vara full av ljus och glädje, men så är det tyvärr inte för alla.
-        Många familjer runt om i Sverige kämpar i vardagen med att få ihop nog för att betala räkningar.
-        Då kan julen lätt kännas som en extra påfrestning. Där har den senaste säsongen med covid-19 skapat ännu större utmaningar för en del. Därför är vi glada för att vi tillsammans kan få ge av det vi har för att sprida ljus och hopp till människor.
+        Julen är en tid som ska vara full av ljus och glädje, men så är det tyvärr inte för alla. Många familjer runt om
+        i Sverige kämpar i vardagen med att få ihop nog för att betala räkningar. Då kan julen lätt kännas som en extra
+        påfrestning. Där har den senaste säsongen med covid-19 skapat ännu större utmaningar för en del. Därför är vi
+        glada för att vi tillsammans kan få ge av det vi har för att sprida ljus och hopp till människor.
       </p>
     </article>
 
--- components/general/Julgranen.vue
+++ components/general/Julgranen.vue
@@ -1,36 +1,23 @@
 <template>
   <div class="w-full my-4 max-w-md relative">
-    <nuxt-image src="/img/julgran-bg-opt.png" class=""/>
-    <nuxt-image v-intersect="isInView" src="/img/julgran-klapp-opt.png" class="julklapp" @click="openDrawer('julklapp')"/>
-    <nuxt-image v-intersect="isInView" src="/img/julgran-matkasse-opt.png" class="matkasse" @click="openDrawer('matkasse')"/>
+    <nuxt-image src="/img/julgran-bg-opt.png" />
+    <XmasEntity src="/img/julgran-klapp-opt.png" type="julklapp" />
+    <XmasEntity src="/img/julgran-matkasse-opt.png" type="matkasse" />
   </div>
 </template>
 
 <script>
-import { EventBus } from '~/utils/EventBus.js';
+import XmasEntity from '~/components/general/XmasEntity'
 
 export default {
-  methods: {
-    openDrawer(category) {
-      EventBus.$emit('isOpen', category);
-    },
-    isInView(entry) {
-      if (entry && entry.target) entry.target.classList.add('tada-delayed');
-    }
-  },
+  components: {
+    XmasEntity
+  }
 }
 </script>
 
 <style lang="postcss" scoped>
 /*! purgecss start ignore */
-.julklapp,
-.matkasse {
-  @apply w-1/2 cursor-pointer transition duration-200 ease-in-out;
-  &:hover {
-    transform: scale3d(1.05, 1.05, 1.05);
-  }
-}
-
 .julklapp {
   @apply absolute left-0 bottom-0 overflow-visible !important;
   animation-delay: 1.5s;
@@ -38,76 +25,6 @@ export default {
 .matkasse {
   @apply absolute right-0 bottom-0 overflow-visible !important;
   animation-delay: 2s;
-}
-
-/* originally authored by Nick Pettit - https://github.com/nickpettit/glide */
-@keyframes pulse {
-  from { transform: scale3d(1, 1, 1); }
-  50% { transform: scale3d(1.1, 1.1, 1.1); }
-  to { transform: scale3d(1, 1, 1); }
-}
-.pulse {
-  animation-name: pulse;
-  animation-duration: .75s;
-  animation-timing-function: ease-in-out;
-}
-/* @keyframes tada {
-  from {
-    transform: scale3d(1, 1, 1);
-  }
-  10%,
-  20% {
-    transform: scale3d(0.9, 0.9, 0.9) rotate3d(0, 0, 1, -3deg);
-  }
-  30%,
-  50%,
-  70%,
-  90% {
-    transform: scale3d(1.1, 1.1, 1.1) rotate3d(0, 0, 1, 3deg);
-  }
-  40%,
-  60%,
-  80% {
-    transform: scale3d(1.1, 1.1, 1.1) rotate3d(0, 0, 1, -3deg);
-  }
-  to {
-    transform: scale3d(1, 1, 1);
-  }
-} */
-@keyframes tadaDelayed {
-  from {
-    transform: scale3d(1, 1, 1);
-  }
-  1%,
-  2% {
-    transform: scale3d(0.9, 0.9, 0.9) rotate3d(0, 0, 1, -3deg);
-  }
-  3%,
-  5%,
-  7%,
-  9% {
-    transform: scale3d(1.1, 1.1, 1.1) rotate3d(0, 0, 1, 3deg);
-  }
-  4%,
-  6%,
-  8% {
-    transform: scale3d(1.1, 1.1, 1.1) rotate3d(0, 0, 1, -3deg);
-  }
-  10%,
-  to {
-    transform: scale3d(1, 1, 1);
-  }
-}
-/* .tada {
-  animation-name: tada;
-  animation-duration: .75s;
-  animation-timing-function: ease-in-out;
-} */
-.tada-delayed {
-  animation-name: tadaDelayed;
-  animation-duration: 7.5s;
-  animation-timing-function: ease-in-out;
-  animation-iteration-count: 5;
 }
 /*! purgecss end ignore */
 </style>
