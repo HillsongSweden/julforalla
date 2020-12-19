5d4db0301b3de62e932d9c5c2e2752a9118acb3d
Sven Parker <sven@svenparker.com>
2020-12-19 22:34:46 +0100

    Fixed sponsor responsivity

--- components/general/Sponsors.vue
+++ components/general/Sponsors.vue
@@ -1,10 +1,10 @@
 <template>
   <div v-if="sponsors.length > 0">
     <h2 class="text-center mb-8">Våra sponsorer</h2>
-    <div class="w-full max-h-32 flex justify-center align-center my-8">
+    <div class="w-full max-h-32 flex flex-wrap justify-center align-center my-8">
       <div
         v-for="sponsor in sponsors" :key="sponsor.id"
-        class="flex-initial inline-flex items-center mx-4 bg-white rounded-lg p-2"
+        class="flex-initial inline-flex items-center m-4 bg-white rounded-lg p-2"
       >
         <nuxt-image
           :src="`/img/${sponsor}`"
